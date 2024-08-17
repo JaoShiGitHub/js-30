79df04ce77eb6ca7db5b47fc287b011c6cc75bc0
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-18 00:00:02 +0700

    2

--- closures/to-be-not-to-be.js
+++ closures/to-be-not-to-be.js
@@ -1,9 +1,21 @@
 // 2704. To Be Or Not To Be
 // https://leetcode.com/problems/to-be-or-not-to-be/description/?envType=study-plan-v2&envId=30-days-of-javascript
 
-const expect = val => {
+const expect = (val) => {
     return {
-        toBe: tbVal => val === tbVal ? `{"value": true}` : `{"error": "Not Equal"}`,
-        notToBe: ntbVal => val !== ntbVal ? `{"value": true}` : `{"error": "Equal"}`
-    }
+        toBe: (tbVal) => {
+            if (val === tbVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: (ntbVal) => {
+            if (val !== ntbVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
 };
